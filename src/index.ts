aecaf4c7698f8cb214a73bb97d7617ca49482b12
Evans Diegbe <devans3001@gmail.com>
2025-05-28 08:32:32 +0100

    cleaning house

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,46 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import OpenAI from "openai";
+import { Hono } from "hono";
+import {cors} from "hono/cors";
+
+
+
+type Bindings = {
+  OPENAI_API_KEY: string;
+  AI:Ai
+}
+
+const app = new Hono<{Bindings:Bindings}>();
+
+
+app.use("*", cors({
+  origin: "*",
+  allowMethods: ["GET", "POST","OPTIONS","PUT"],
+  allowHeaders: ["Content-Type", "Authorization","X-Custom-Header"],
+  credentials: true,
+  maxAge: 600,
+exposeHeaders: ["Content-Length", "X-Request-ID"],
+
+}));
+
+app.post("/translateDocument", async (c) => {
+  const { docData, targetLanguage } = await c.req.json();
+
+  const sum = await c.env.AI.run("@cf/facebook/bart-large-cnn",{
+	input_text: docData,
+	max_length: 1024,
+  })
+
+  const response = await c.env.AI.run("@cf/meta/m2m100-1.2b", {
+	text:sum.summary,
+	source_lang:"english",
+	target_lang: targetLanguage,
+  })
+
+  return new Response(JSON.stringify(response))
+
+})
+
+export default app;
